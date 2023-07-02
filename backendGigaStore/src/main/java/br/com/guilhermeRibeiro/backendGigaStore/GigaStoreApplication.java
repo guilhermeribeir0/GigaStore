34e941309a458bbe97add56970723c31f5d966c2
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2023-07-02 10:44:42 -0300

    adiciona base backend ao projeto

---
+++ backendGigaStore/src/main/java/br/com/guilhermeRibeiro/backendGigaStore/GigaStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermeRibeiro.backendGigaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigaStoreApplication.class, args);
+	}
+
+}
---
+++ backendGigaStore/src/test/java/br/com/guilhermeRibeiro/backendGigaStore/GigaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermeRibeiro.backendGigaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
