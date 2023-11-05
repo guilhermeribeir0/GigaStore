39743cac32d8b37a11426ecf67182fb6ac296689
Guilherme Ribeiro <dev.guilhermeribeiro@gmail.com>
2023-11-05 10:47:39 -0300

    inicia config de venda

--- backendGigaStore/src/main/java/br/com/guilhermeRibeiro/backendGigaStore/domain/controller/VendaController.java
+++ backendGigaStore/src/main/java/br/com/guilhermeRibeiro/backendGigaStore/domain/controller/VendaController.java
@@ -1,4 +1,15 @@
 package br.com.guilhermeRibeiro.backendGigaStore.domain.controller;
 
+import br.com.guilhermeRibeiro.backendGigaStore.domain.service.VendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/vendas")
 public class VendaController {
+
+    @Autowired
+    private VendaService vendaService;
+
 }
--- backendGigaStore/src/main/java/br/com/guilhermeRibeiro/backendGigaStore/domain/service/VendaService.java
+++ backendGigaStore/src/main/java/br/com/guilhermeRibeiro/backendGigaStore/domain/service/VendaService.java
@@ -1,8 +1,11 @@
 package br.com.guilhermeRibeiro.backendGigaStore.domain.service;
 
+import br.com.guilhermeRibeiro.backendGigaStore.domain.entity.Venda;
 import br.com.guilhermeRibeiro.backendGigaStore.domain.repository.VendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VendaService {
@@ -13,5 +16,10 @@ public class VendaService {
     private ClienteService clienteService;
     @Autowired
     private ProdutoService produtoService;
+
+    public List<Venda> listarVendas() {
+        List<Venda> vendas = vendaRepository.findAll();
+        return vendas;
+    }
 
 }
